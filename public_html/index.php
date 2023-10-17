0e9ab3f644e21eb4e7cce77bd916694a25c08c51
Riki Krismawan <krismawan4@gmail.com>
2023-10-17 16:07:25 +0700

    port setting

--- public_html/index.php
+++ public_html/index.php
@@ -4,12 +4,12 @@
 // I assume that you've created a new username and password
 $host = 'mysql';
 $user = 'user';
-$pass = 'user';
+$pass = 'root';
 $conn = new mysqli($host, $user, $pass);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-  echo "Connected to MySQL successfully!";
+    echo "Connected to MySQL successfully!";
 }
 ?>
